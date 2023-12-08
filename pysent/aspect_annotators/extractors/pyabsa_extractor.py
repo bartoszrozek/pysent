5c03320a96bf39a40087bcfe905714cf81c25026
bartoszrozek <bartoszrozek@github.com>
2023-12-08 23:45:16 +0100

    pyabsa extractor implementation

--- pysent/aspect_annotators/extractors/pyabsa_extractor.py
+++ pysent/aspect_annotators/extractors/pyabsa_extractor.py
@@ -1,70 +1,61 @@
 """
 Sentiment classifier based on the PyABSA Python package.
 """
-from openai import OpenAI
-import openai
-import time
+from pyabsa import AspectTermExtraction as ATEPC, available_checkpoints
+
 
 from pysent.aspect_annotators.extractors.aspect_extractor import AspectExtractor
 from pysent.data_structures import ExtractedAspect
 
 
 class PyabsaExtractor(AspectExtractor):
-    def __init__(self, api_key: str, free_tier: bool = True):
-        openai.api_key = api_key
-        self.free_tier = free_tier
+    def __init__(self, n_neighbors: int = 4):
+        """Object constructor
+
+        Parameters
+        ----------
+        n_neighbors : int, optional
+            Number of surroding words to be taken while extracting context,
+            by default 4
+
+        Raises
+        ------
+        ValueError
+            Error is language not supported
+        """
+        checkpoint_map = available_checkpoints()
+
+        self.classifier = ATEPC.AspectExtractor(
+            "multilingual",
+            auto_device=False,  # True,  # False means load model on CPU
+            cal_perplexity=True,
+        )
+        self.n_neighbors = n_neighbors
 
     def extract(self, texts: list[str]) -> list[list[ExtractedAspect]]:
         super().check_arguments(texts)
+
+        tool_annotations = self.classifier.predict(
+            texts,
+            save_result=False,
+            print_result=False,  # print the result
+            ignore_error=True,  # ignore the error when the model cannot predict the input
+        )
         aspects = []
 
-        for text in texts:
-            text_aspects = []
-            message = [
-                {
-                    "role": "system",
-                    "content": f"""For Text below provide me it's distinct aspects - subjects present in the text, that can be later used for aspect based sentiment analysis. 
-                You can return one or multiple aspects, but they shouldn't repeat. 
-                If there are two chunks about the same thing in the text, you should find a way to distinguish them in the aspect name.
-                Keep the aspects concise (no more than 3 words). 
-                Besides the aspect, provide the chunk of the text that describes that aspect. Chunks should sum up to the entire sentence.
-                If you are returning one aspect, use the format:
-                Aspect: <aspect you suggest>
-                Chunk: <chunk of the Text about the aspect>
-
-                If you are returning multiple aspects, use the format:
-                Aspect: <aspect you suggest>
-                Chunk: <piece of the Text about the aspect>
-
-                Aspect: <aspect you suggest>
-                Chunk: <piece of the Text about the aspect>
-                ... and so on.
-                Don't repeat the text or provide any additional output. Answer should be in the same language as the input.
-
-                Text:
-                {text}
-                """,
-                }
-            ]
-            chat = OpenAI(api_key=openai.api_key).chat
-            chat_completion = chat.completions.create(
-                messages=message,
-                model="gpt-3.5-turbo",
+        for anotation in tool_annotations:
+            aspects.append(
+                [
+                    ExtractedAspect(
+                        aspect=aspect,
+                        text=self.get_neighbors(
+                            main_word=aspect,
+                            full_text=anotation["sentence"],
+                            n_neighbors=self.n_neighbors,
+                        ),
+                    )
+                    for aspect in anotation["aspect"]
+                ]
             )
-            reply = chat_completion.choices[0].message.content
-            try:
-                aspects_list = []
-                split_by_aspects = reply.split("\n\n")
-                for aspect_chunk in split_by_aspects:
-                    aspect, chunk = aspect_chunk.split("\n")
-                    aspect = aspect.removeprefix("Aspect: ")
-                    chunk = chunk.removeprefix("Chunk: ")
-                    text_aspects.append(ExtractedAspect(aspect, chunk))
 
-                if self.free_tier:
-                    time.sleep(20)
-            except:
-                ValueError(f"Something wrong in the response: {reply}!")
-
-            aspects.append(text_aspects)
         return aspects
